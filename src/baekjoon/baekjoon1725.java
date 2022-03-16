74b1aec85ddc7ad4112935b8942d293cf253013b
KangJungMin <aaggf1112@gmail.com>
2022-03-16 20:44:56 +0900

    Update baekjoon1725.java

--- src/baekjoon/baekjoon1725.java
+++ src/baekjoon/baekjoon1725.java
@@ -10,26 +10,25 @@ public class baekjoon1725 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num =  Integer.parseInt(br.readLine());
-		int[] arr = new int[num+2];
-		int result =0;
+	int[] arr = new int[num+2];
+	int result =0;
         
-		for(int i=1; i<num+1; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
+	for(int i=1; i<num+1; i++) {
+		arr[i] = Integer.parseInt(br.readLine());
+	}
 		
-		Stack<Integer> stack = new Stack<>();
-		stack.push(0);
+	Stack<Integer> stack = new Stack<>();
+	stack.push(0);
         
-		for(int i=1; i<num+2; i++) {
-			while(!stack.isEmpty()) {
-				int top = stack.peek();
-				if(arr[top] <= arr[i]) break;
-				stack.pop();
-				result = Math.max(result, arr[top]*(i-stack.peek()-1));
-			}
-			stack.push(i);
+	for(int i=1; i<num+2; i++) {
+		while(!stack.isEmpty()) {
+			int top = stack.peek();
+			if(arr[top] <= arr[i]) break;
+			stack.pop();
+			result = Math.max(result, arr[top]*(i-stack.peek()-1));
 		}
-		
-		System.out.println(result);
+		stack.push(i);
+	}
+	System.out.println(result);
     }
 }
