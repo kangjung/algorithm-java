a151ca4ece0dc299a9f9bd0cfb9eaaf5d6fb448b
KangJung <aaggf1112@gmail.com>
2022-10-15 22:12:37 +0900

    [leetcode] 1531. String Compression II

--- src/leetcode/leetcode1531.java
+++ src/leetcode/leetcode1531.java
@@ -1,32 +1,43 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//https://leetcode.com/problems/string-to-integer-atoi/
+//https://leetcode.com/problems/string-compression-ii/
+//1531. String Compression II
 public class leetcode1531 {
 	public static void main(String[] args) {
-		System.out.println(new leetcode1531().myAtoi("42"));
-		System.out.println(new leetcode1531().myAtoi("   -42"));
-		System.out.println(new leetcode1531().myAtoi("4193 with words"));
+		System.out.println(new leetcode1531().getLengthOfOptimalCompression("aaabcccd", 2));
+		System.out.println(new leetcode1531().getLengthOfOptimalCompression("aabbaa", 2));
+		System.out.println(new leetcode1531().getLengthOfOptimalCompression("aaaaaaaaaaa", 0));
 	}
 
-	private int myAtoi(String s) {
-		int result = 0;
-
-		s = s.stripLeading();
-
-		Pattern pattern = Pattern.compile("^[+-]?[0-9]+");
-		Matcher matcher = pattern.matcher(s);
-
-		if(matcher.find()){
-			try{
-				result = Integer.parseInt(matcher.group());
-			}catch(Exception e){
-				System.out.println(result);
-				return s.startsWith("-") ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+	public int getLengthOfOptimalCompression(String s, int k) {
+		int result = 0, cnt = 0, diffCnt = 0;
+		int length = s.length();
+		int[][] arr = new int[length + 1][k + 1];
+		for (int i = 0; i <= length; i++) {
+			Arrays.fill(arr[i], Integer.MAX_VALUE >> 1);
+		}
+		arr[0][0] = 0;
+		for (int i = 1; i <= length; i++) {
+			for (int j = 0; j <= k ; j++) {
+				if (j > 0) {
+					arr[i][j] = arr[i - 1][j - 1];
+				}
+				cnt = 0;
+				diffCnt = 0;
+				for (int m = i; m >= 1 && diffCnt <= j; m--) {
+					if (s.charAt(m - 1) == s.charAt(i - 1)) {
+						cnt++;
+						arr[i][j] = Math.min(arr[i][j], arr[m - 1][j - diffCnt] + ((cnt == 1) ? 1 : (cnt < 10) ? 2 : (cnt < 100) ? 3 : 4));
+					} else
+						diffCnt++;
+				}
 			}
 		}
+		result = arr[length][k];
 		return result;
 	}
 }
