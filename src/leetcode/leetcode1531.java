ce3d480c25690a57313a957078159b0ca758550e
KangJung <aaggf1112@gmail.com>
2022-10-15 22:04:19 +0900

    [leetcode] 1531. String Compression II

---
+++ src/leetcode/leetcode1531.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//https://leetcode.com/problems/string-to-integer-atoi/
+public class leetcode1531 {
+	public static void main(String[] args) {
+		System.out.println(new leetcode1531().myAtoi("42"));
+		System.out.println(new leetcode1531().myAtoi("   -42"));
+		System.out.println(new leetcode1531().myAtoi("4193 with words"));
+	}
+
+	private int myAtoi(String s) {
+		int result = 0;
+
+		s = s.stripLeading();
+
+		Pattern pattern = Pattern.compile("^[+-]?[0-9]+");
+		Matcher matcher = pattern.matcher(s);
+
+		if(matcher.find()){
+			try{
+				result = Integer.parseInt(matcher.group());
+			}catch(Exception e){
+				System.out.println(result);
+				return s.startsWith("-") ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+			}
+		}
+		return result;
+	}
+}
