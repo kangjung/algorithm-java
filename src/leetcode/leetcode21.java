e44bca05668e4d0a6f09903bc89cc56f69bc6b6f
KangJung <aaggf1112@gmail.com>
2022-10-28 23:57:43 +0900

    [leetcode] 21. Merge Two Sorted Lists

---
+++ src/leetcode/leetcode21.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+//https://leetcode.com/problems/flood-fill
+//21. Merge Two Sorted Lists
+public class leetcode21 {
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		if (list1 == null) {
+			return list2;
+		}
+		if (list2 == null) {
+			return list1;
+		}
+		if (list1.val < list2.val) {
+			list1.next = mergeTwoLists(list1.next, list2);
+			return list1;
+		} else {
+			list2.next = mergeTwoLists(list1, list2.next);
+			return list2;
+		}
+	}
+}
