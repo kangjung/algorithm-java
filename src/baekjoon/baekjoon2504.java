020099ee8fff7e990bc652add05adaed56297a10
kangjung <aaggf1112@gmail.com>
2025-03-31 19:04:20 +0900

    [baekjoon] 2504. 괄호의 값

--- src/baekjoon/baekjoon2504.java
+++ src/baekjoon/baekjoon2504.java
@@ -7,62 +7,43 @@ import java.util.*;
 public class baekjoon2504 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Stack<String> stack = new Stack<>();
 		String s = br.readLine();
-		String[] arr = s.split("");
-		int result = 0;
+		Stack<Character> stack = new Stack<>();
+		Stack<Integer> values = new Stack<>();
+
+		int temp = 1;
 		int sum = 0;
 
-		if(!"]".equals(arr[0]) && !")".equals(arr[0])){
-			loopOut:
-			for(int i=0; i<arr.length; i++) {
-				String next = arr[i];
-				if("(".equals(next) || "[".equals(next)){
-					stack.push(next);
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+
+			if (ch == '(') {
+				stack.push(ch);
+				values.push(temp);
+				temp *= 2;
+			} else if (ch == '[') {
+				stack.push(ch);
+				values.push(temp);
+				temp *= 3;
+			} else if (ch == ')') {
+				if (stack.isEmpty() || stack.peek() != '(') {
+					System.out.println(0);
+					return;
 				}
-				if(")".equals(next)){
-					sum = 0;
-					while(!stack.isEmpty()) {
-						String prev = stack.peek();
-						if (prev.equals("(")){
-							stack.pop();
-							stack.push(String.valueOf(sum == 0 ? 2  : (sum * 2)));
-							break;
-						} else if (prev.equals("[")){
-							break loopOut;
-						} else { 
-							sum += Integer.parseInt(stack.pop());
-						}
-					}
-				} else if ("]".equals(next)){
-					sum = 0;
-					
-					while(!stack.isEmpty()) {
-						String prev = stack.peek();
-						if (prev.equals("[")){
-							stack.pop();
-							stack.push(String.valueOf(sum == 0 ? 3  : (sum * 3)));
-							break;
-							
-						} else if (prev.equals("(")){
-							break loopOut;
-						} else { 
-							sum += Integer.parseInt(stack.pop());
-						}
-					}
+				if (s.charAt(i - 1) == '(') sum += temp;
+				stack.pop();
+				temp = values.pop();
+			} else if (ch == ']') {
+				if (stack.isEmpty() || stack.peek() != '[') {
+					System.out.println(0);
+					return;
 				}
+				if (s.charAt(i - 1) == '[') sum += temp;
+				stack.pop();
+				temp = values.pop();
 			}
 		}
-		
-		while(!stack.isEmpty()) {
-			String peek = stack.peek();
-			if (")".equals(peek) || "]".equals(peek) || "(".equals(peek) || "[".equals(peek)) {
-				result = 0;
-				break;
-			}
-			result += Integer.parseInt(peek);
-			stack.pop();
-		}
-		System.out.println(result);
+
+		System.out.println(stack.isEmpty() ? sum : 0);
 	}
 }
